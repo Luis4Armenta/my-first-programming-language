4cb373f9bad5ed6d3d53f9f106c185c5fef4d90e
Luis Armenta <luigiangelxd@gmail.com>
2023-02-01 13:49:29 -0600

    Entry point

---
+++ main.py
@@ -0,0 +1,10 @@
+from lp.repl import start_repl
+
+def main() -> None:
+  print('Bienvenido al nuestro lenguaje de progración.')
+  print('Escribe una oración para comenzar.')
+  
+  start_repl()
+
+if __name__ == '__main__':
+  main()
