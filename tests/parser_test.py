330934a8b880f0c4a3c914a8a4486f9c6166d4f5
Luis Armenta <luigiangelxd@gmail.com>
2023-02-01 19:59:42 -0600

    LetStatments

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -1,13 +1,15 @@
 from unittest import TestCase
+from typing import List
 
 from lp.lexer import Lexer
 from lp.parser import Parser
-from lp.ast import Program
+from lp.ast import Program, LetStatement, Statement, Identifier
+from lp.token import Token, TokenType
 
 class ParserTest(TestCase):
 
   def test_parse_program(self) -> None:
-    source: str = 'variable x = 5'
+    source: str = 'variable x = 5;'
     lexer: Lexer = Lexer(source)
     parser: Parser = Parser(lexer)
     
@@ -15,4 +17,44 @@ class ParserTest(TestCase):
     
     self.assertIsNotNone(program)
     self.assertIsInstance(program, Program)
+    
+  def test_let_statements(self) -> None:
+    source: str = '''
+      variable x = 5;
+      variable y = 10;
+      variable foo = 20;
+    '''
+    lexer: Lexer = Lexer(source)
+    parser: Parser = Parser(lexer)
+    
+    program: Program = parser.parse_program()
+    
+    self.assertEquals(len(program.statements), 3)
+    
+    for statement in program.statements:
+      self.assertEqual(statement.token_literal(), 'variable')
+      self.assertIsInstance(statement, LetStatement)
+      
+  def test_correct_let_statements_identities(self) -> None:
+    source: str = '''
+      variable x = 5;
+      variable y = 10;
+      variable foo = 20;
+    '''
+    lexer: Lexer = Lexer(source)
+    parser: Parser = Parser(lexer)
+    
+    program: Program = parser.parse_program()
+    
+    identifiers: List[str] = []
+    for statement in program.statements:
+      self.assertIsInstance(statement, LetStatement)
+      identifiers.append(str(statement).split(' ')[1])
+    
+    expected_identifiers: List[str] = [
+      'x', 'y', 'foo'
+    ]
+    
+    self.assertEquals(identifiers, expected_identifiers)
+
 
